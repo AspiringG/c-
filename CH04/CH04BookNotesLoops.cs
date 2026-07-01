738ef729e400f4f68d69d4a1eb01495e0f582889
Farid Mammadli <faridapril1982@gmail.com>
2026-07-01 23:22:54 +0400

    Added Explanations for While and For loops

--- CH04/CH04BookNotesLoops.cs
+++ CH04/CH04BookNotesLoops.cs
@@ -16,7 +16,48 @@ namespace Loops
 
                                             // Loop Types
 
+            for (int month = 1; month <= 12; month++)
+            {
+                // month is the initializer, and then we have a condition month <= 12, which when
+                // it evaluates to true, the loop continues, otherwise loop stops.
+                // The iterator runs at the end of each loop iteration, updating the loop variable 
+                
+                Console.WriteLine($"Processing statement for month {month}"); // $ is an interpolated string allowing us to embed any expression within brackets we like 
+                
+                // Our use above causes C# to evaluate the value of month and insert it at runtime.
+            }
             
+
+                                            // While Loop
+
+            // While loop is a condition based looping construct that continues to execute as long as
+            // the specified condition evaluates to true. This is a more flexible loop than the for loop,
+            // and it requires caution to avoid infinite loops.
+
+            // Infinite loop: A loop whose condition always evaluates to true, and there is no code that 
+            // the loop to end
+            
+
+            // Below Code continues to run until the condition balance > 0 is no longer
+            // evaluating to true. Balance is initially 1000. MonthlyWithdrawal is 100. 
+
+            // Expected outcome should be Funds depleted after 10 months. Where balance - withdrawAmount will repeat 10 times.
+            // Updating the months variable
+
+            decimal balance = 1000.00m;
+            decimal monthlyWithdrawal = 100.00m;
+            int months = 0;
+
+            while(balance > 0)
+            {
+                balance -= monthlyWithdrawal;
+                months++;
+            }
+
+            Console.WriteLine($"Funds depleted after {months} months.");
+            
+
+
         }
     }
 }
