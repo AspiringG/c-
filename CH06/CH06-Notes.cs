2739faf04c314e4f24e8c522fe444f65ff922877
Farid Mammadli <faridapril1982@gmail.com>
2026-07-05 20:22:19 +0400

    MultiDimensional Arrays added

--- CH06/CH06-Notes.cs
+++ CH06/CH06-Notes.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 
 namespace CH06Notes 
 {
@@ -22,15 +23,82 @@ namespace CH06Notes
             scores[4] = 55;
 
             // Alternate
-            int[] scores = {11, 23, 44, 11, 99};
+            int[] scoress = {11, 23, 44, 11, 99};
 
             string player1 = "New";
             string player2 = "Farid";
             string player3 = "NA";
 
             string[] team = {player1, player2, player3};
+
+
+
+            // Modifying the arrays and their access;
+
+            int[] scoresa = {80, 123, 31, 41, 50};
+            int firstScore = scores[0];
+            int secondScore = scores[1];
+
             
-            
+            // Modifying Array Elements
+
+            for(int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine("Score: " + i + ": " + scores[i]);
+            }
+
+            foreach(int score in scores)
+            {
+                Console.WriteLine("The score is: " + score);
+            }
+
+//                  Multidimensional Arrays are also known as recrangular arrays
+
+            // int[,] matrix = new int[3,2]; // 3 rows and 2 columns
+            int[,] grid =
+            {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90}
+            };
+
+            int[,] matrixd =
+            {
+                {3, 1},
+                {2, 2},
+                {1, 1}
+            };
+
+           for(int row = 0; row < grid.GetLength(0); row++)
+            {
+                for (int col = 0; col < grid.GetLength(1); col++)
+                {
+                    Console.Write(grid[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            for(int row = 0; row < matrixd.GetLength(0); row++)
+            {
+                for(int col = 0; col < matrixd.GetLength(1); col++)
+                {
+                    Console.Write(matrixd[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            int[,] table = new int[10,10];
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    table[i, j] = (i + 1) * (j + 1);
+                    Console.Write(table[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            }
         }
     }
-}
