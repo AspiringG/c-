ade1e54eb8aac26483d8476262cd95135135efe9
Farid Mammadli <faridapril1982@gmail.com>
2026-07-07 17:04:05 +0400

    Chapter 8 started

---
+++ CH08/RVProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RVProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare and instantiate an RV object
+
+            /*
+
+                
+
+            */
+            RV myRV = new()
+            {
+                // Assign the data to the object's properties
+                VIN = "2Y7C45678M2025RV",
+                EngineType = "Gasoline"
+            };
+
+            myRV.startEngine();
+        }
+    }
+}
